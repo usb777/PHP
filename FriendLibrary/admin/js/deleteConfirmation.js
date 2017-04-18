f5d515b1465f881f59c902c84187f1ae46d8d69c
usb777 <dzsamoila@gmail.com>
2017-04-17 22:53:23 -0800

    Add files via upload

---
+++ FriendLibrary/admin/js/deleteConfirmation.js
@@ -0,0 +1,79 @@
+﻿                 // delete file from User
+            function getDeleteConfirmationUserBook(b_id)
+			{
+               var path = window.location.protocol+"//"+window.location.host+"/admin/";
+				
+                var retVal = confirm("Do you want to DELETE this book? book_id="+b_id);
+                
+				if( retVal == true )
+				{
+                   window.location=path+"Controller/BooksController.php?b_id="+b_id+"&action=deleteBookByUser";
+                   return true;
+                }
+                else
+				    {                   
+                      return false;
+                    }
+             }
+            
+		   
+		   
+		 /* this function ask about delete Book, from admin*/
+            function getdeleteBookConfirmationSA(b_id)
+            {
+				 var path = window.location.protocol+"//"+window.location.host+"/admin/";
+				
+                var retVal = confirm("Do you want to DELETE this book? book_id="+b_id);
+                if( retVal == true ){
+                   window.location=path+"Controller/BooksController.php?b_id="+b_id+"&action=deleteBookByAdmin";
+                   return true;
+                }
+                else
+				    {
+                   
+                      return false;
+                    }
+             } // end of function
+       
+    
+      
+            
+            
+            /* this function ask about delete User info, from admin*/
+            function getdeleteUsersConfirmationSA(user_id)
+            {
+				 var path = window.location.protocol+"//"+window.location.host+"/admin/";
+				
+                var retVal = confirm("Do you want to DELETE data of this user ? user_id="+user_id);
+                if( retVal == true ){
+                   window.location=path+"Controller/UsersController.php?u_id="+user_id+"&action=deleteUser";
+                   return true;
+                }
+                else{
+                   // document.write ("User does not want to continue!");
+                   return false;
+                }
+             } // end of function
+			 
+			 
+			 
+			   /* this function ask about delete Category info, from admin*/
+            function getDeleteCategoriesConfirmation(category_id)
+            {
+				var path = window.location.protocol+"//"+window.location.host+"/admin/";
+				
+                var retVal = confirm("Do you want to DELETE category data ? category_id="+category_id);
+                if( retVal == true )
+				{
+                  
+             	    window.location=path+"Controller/CategoryController.php?category_id="+category_id+"&action=deleteCategory";
+                   return true;
+                }
+                else{
+                   // document.write ("User does not want to continue!");
+                   return false;
+                }
+             } // end of function
+			 
+			 
+        
---
+++ FriendLibrary/admin/js/updateConfirmation.js
@@ -0,0 +1,80 @@
+﻿/* this function ask about update BOOK info, from User*/    
+        function getUpdateConfirmationUserBook(b_id)
+		{      
+		       var path = window.location.protocol+"//"+window.location.host+"/admin/";
+			   
+               var retVal = confirm("Do you want UPDATE this book info ? book id="+b_id);
+               if( retVal == true )
+			   {
+                  window.location=path+"backendUpdateBook.php?b_id="+b_id+"&action=updateBookByUser";
+            	  
+                  return true;
+               }
+               else
+			   {
+                 
+                  return false;
+               }
+            }   // end of function
+
+
+
+
+/* this function ask about update BOOK info, from SUPER admin*/    
+        function getUpdateConfirmationUserBookSA(b_id)
+		{      
+		       var path = window.location.protocol+"//"+window.location.host+"/admin/";
+			   
+               var retVal = confirm("Mr.Admin, Do you want UPDATE this book info ? book id="+b_id);
+               if( retVal == true )
+			   {
+                  window.location=path+"backendSAUpdateBook.php?b_id="+b_id+"&action=updateBookByAdmin";
+            	  
+                  return true;
+               }
+               else{
+                 
+                  return false;
+               }
+            }   // end of function
+       
+       
+       
+    /* this function ask about update User info, from admin*/
+            function getUpdateUsersConfirmationSA(user_id)
+            { 
+			    var path = window.location.protocol+"//"+window.location.host+"/admin/";
+				
+                var retVal = confirm("Do yoU want to UPDATE data of this user ? user_id="+user_id);
+                
+				if( retVal == true ){
+                  
+             	   window.location=path+"backendSAUpdateUser.php?u_id="+user_id+"&action=updateUser";
+                   return true;
+                }
+                else{
+                
+                   return false;
+                }
+             } // end of function
+            
+            
+            
+            /* this function ask about update Category info, from admin*/
+            function getUpdateCategoriesConfirmation(category_id)
+            {
+				
+				var path = window.location.protocol+"//"+window.location.host+"/admin/";
+				
+                var retVal = confirm("Do you want to UPDATE category data ? category_id="+category_id);
+                if( retVal == true ){
+                  
+             	   window.location=path+"backendSAUpdateInsertCategory.php?category_id="+category_id+"&action=updateCategory";
+                   return true;
+                }
+                else{
+                  
+                   return false;
+                }
+             } // end of function
+            
